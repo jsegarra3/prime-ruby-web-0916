9e51e08841b467f1293c82c396dec8a7469e6129
James Segarra <jamessegarra3@gmail.com>
2016-09-07 19:07:20 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+def prime?(num)
+  return false if num <= 1
+  i=0
+  tempArray=[]
+  finalAnswer = false
+    while i<num-1 #creates array with all values 1 through num, except for num itself
+    tempArray[i]=i+1
+    i+=1
+  end
+
+  tempArray.each do |j|
+    if (j==1)
+      finalAnswer = true
+    elsif (num%j==0) #if num itself is in the array, this will create a false positive. Better to ensure while loop never put it in
+      finalAnswer = false
+    else
+      #just do nothing
+    end
+  end
+  return finalAnswer
+end
